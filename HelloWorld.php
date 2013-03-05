0a422ace4edf721745e8dd9c19092fe84c1ffdbd
Ken.Akiyoshi <x002dc@gmail.com>
2013-03-05 15:19:14 +0900

    OK

--- HelloWorld.php
+++ HelloWorld.php
@@ -16,7 +16,7 @@ class HelloWorld
     {
         $sql = "INSERT INTO hello VALUES (" . $this->pdo->quote($what) . ")";
         $this->pdo->query($sql);
-        return "Helloooo $what";
+        return "Hello $what";
     }
 
 
